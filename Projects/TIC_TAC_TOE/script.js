80eb243b4ab5b3956a49893157489ecec90fdf80
pumpumx <trishuchibhot14@gmail.com>
2024-12-29 20:27:51 +0530

    Basic Styling

--- Projects/TIC_TAC_TOE/script.js
+++ Projects/TIC_TAC_TOE/script.js
@@ -3,14 +3,16 @@ let player1 , player2;
 let turn = 0;
 function main(id){
     if(turn==0){
+        document.getElementById("Player").innerHTML="Player 2 Turn";
         document.getElementById(id).style.backgroundColor="red";
         turn=1;
     }
     else if(turn==1){
+        document.getElementById("Player").innerHTML="Player 1 Turn";
         document.getElementById(id).style.backgroundColor="green";
         turn=0; 
     }
 }
 function logic(){
-
+ 
 }
