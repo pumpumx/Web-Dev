7a5d12d57fb39d2e1c1ed774498a2c0c2781107e
pumpumx <trishuchibhot14@gmail.com>
2024-12-29 21:43:09 +0530

    Debugging Code

--- Projects/TIC_TAC_TOE/script.js
+++ Projects/TIC_TAC_TOE/script.js
@@ -1,18 +1,28 @@
-
-let player1 , player2;
+let boxes= document.querySelectorAll(".item");
 let turn = 0;
+let arr1 = [[1,2,3],[1,4,7],[0,4,7],[0,4,8],[2,4,6],[2,5,8]];
 function main(id){
-    if(turn==0){
+    if(turn==0 && document.getElementById(id).disabled!=true){
         document.getElementById("Player").innerHTML="Player 2 Turn";
         document.getElementById(id).style.backgroundColor="red";
+        document.getElementById(id).disabled=true;
         turn=1;
     }
-    else if(turn==1){
+    else if(turn==1 && document.getElementById(id).disabled!=true){
         document.getElementById("Player").innerHTML="Player 1 Turn";
         document.getElementById(id).style.backgroundColor="green";
+        document.getElementById(id).disabled=true;
         turn=0; 
     }
 }
-function logic(){
- 
+
+let winner = ()=>{
+    for(pattern of arr1){
+        let pos1 = boxes[pattern[0]];
+        let pos2 = boxes[pattern[1]];
+        let pos3 = boxes[pattern[2]];
+    }
+    // if(pos1!="" && pos2!="" && pos3!=""){
+    //     if(pos1)
+    // }
 }
