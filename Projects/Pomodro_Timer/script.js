ea6e5556b21bea253df286c59c78aaab0784f282
pumpumx <trishuchibhot14@gmail.com>
2025-01-01 16:06:02 +0530

    Pomodro Timer Completed

--- Projects/Pomodro_Timer/script.js
+++ Projects/Pomodro_Timer/script.js
@@ -1,29 +1,52 @@
 let start = document.getElementById("start");
-let stopit = document.getElementById("stop");
-let reset = document.getElementById("reset");
+let stopbutton = document.getElementById("stop");
+let resetbutton = document.getElementById("reset");
 let interval;
-start.addEventListener("click" , startclock());
-stopit.addEventListener("click" , stopclock());
-reset.addEventListener("click", resetclock());
+start.addEventListener("click" , startclock);
+stopbutton.addEventListener("click" , stopit);
+resetbutton.addEventListener("click", reset);
 
 function startclock(){
+    start.disabled=true;
     let second = document.getElementById("second");
     let minute = document.getElementById("minute");
 
     interval = setInterval(() => {
-        if(Number(second.value)==0){
-            minute.value = minute.value-1;
+        let secValue = parseInt(second.value);
+        let minValue = parseInt(minute.value);
+        if(secValue===0){
+            if(minValue>0){
+            secValue=59;
+            minValue = minValue -1;
+            }
+            else{
+                clearInterval(interval);
+                alert("Timer's up");
+            }
         }
         else{
-            second.value--;
+            secValue = secValue -1;
         }
+
+        if(secValue<10){
+            secValue = formatString(secValue); 
+        }
+        second.value = secValue;
+        minute.value = minValue;
     }, 1000)
 }
 
-function stop(){
+function stopit(){
+    start.disabled = false;
     clearInterval(interval);
 }
 
 function reset(){
     clearInterval(interval);
+    document.getElementById("minute").value = "25"; 
+    document.getElementById("second").value = "00"; 
+}
+
+function formatString(value){
+    return String(value).padStart(2,"0");
 }
