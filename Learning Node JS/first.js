d2d21e26ffcb34f6a0c550a48c80fdb8011dd13a
pumpumx <trishuchibhot14@gmail.com>
2025-01-07 05:28:33 +0530

    Learn Yourself

--- Learning Node JS/first.js
+++ Learning Node JS/first.js
@@ -1,7 +1,10 @@
-let http = require("http");
+// let http = require("http");
 
-http.createServer(function(req,res){
-    res.writeHead(200,{'Content-type': 'text/html'});
-    res.write(req.url);
-    res.end();
-}).listen(8080);
+// http.createServer(function(req,res){
+//     res.writeHead(200,{'Content-type': 'text/html'});
+//     res.write(req.url);
+//     res.end();
+// }).listen(8080);
+
+const os = require('os');
+console.log(os.hostname());
