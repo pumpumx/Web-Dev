a74a2fd9a080e97c54b05b50feeb663bc21848a3
pumpumx <trishuchibhot14@gmail.com>
2025-01-12 18:56:19 +0530

    Note concept

---
+++ Projects/Note_Taking_App/script.js
@@ -0,0 +1,14 @@
+var add = document.getElementById("copy");
+var box = document.getElementById("dup");
+var main2 = document.getElementById("mainGrid");
+box.addEventListener("click" , main);
+
+function main(){
+    let mem = document.createElement("div");
+    main2.appendChild("mem");
+    mem.classList.add("item");
+    mem.style.width = "400px";
+    mem.style.width = "400px";
+
+
+}
