f9c0d2d9c74505766599aa29ad74b59c335c028f
pumpumx <trishuchibhot14@gmail.com>
2025-01-21 18:00:11 +0530

    New Pass gen

--- React/Projects/BgChanger/src/App.jsx
+++ React/Projects/BgChanger/src/App.jsx
@@ -3,7 +3,10 @@ import './App.css'
 function App() {
   const [color , setColor] = useState("olive")
   return (
-    <>  
+    <> 
+    <head>
+        <title>BgChanger</title>
+    </head> 
         <div className="h-screen w-full duration-200" style={{backgroundColor:color}}>
           <div className=" rounded-2xl justify-around gap-3 fixed bottom-12 flex flex-wrap h-12  inset-2 p-2 m">
                 <button className='bg-white rounded-xl outline-none shadow-lg w-24' onClick={()=> setColor("Red")}>Red</button>
