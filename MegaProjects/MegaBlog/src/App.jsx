b52ae5d1d818cec8ed79232a50720fec6d7c9c44
pumpumx <trishuchibhot14@gmail.com>
2025-03-09 01:17:07 +0530

    env setup completed

--- MegaProjects/MegaBlog/src/App.jsx
+++ MegaProjects/MegaBlog/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  console.log(import.meta.env.VITE_APPWRITE_URL)
   return (
     <>
     <h1 className=''>App Running</h1>
