a4ce32d42ac9600c0bec5b7a3be81e0c6c3497f5
pumpumx <trishuchibhot14@gmail.com>
2025-03-09 17:33:43 +0530

    Configuration done

---
+++ MegaProjects/MegaBlog/src/appwrite/configuration.js
@@ -0,0 +1,104 @@
+import { configs } from "eslint-plugin-react-hooks";
+import config from "../config/config";
+import {Client , ID , Databases , Storage , Query} from "appwrite"
+
+export class Services{
+
+    client = new Client();
+    databases;
+    storage;
+    query;
+    bucket;
+
+    constructor(){
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId)
+
+        this.databases = new Databases(this.client)
+        this.bucket = new Storage(this.client)
+    }
+
+    async createPost({title , slug ,content ,featuredImage , status , userId}){
+        try {
+            return await this.databases.createDocument(
+                config.appwriteDataBaseId ,
+                config.appwriteCollectionId ,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service :: error :: ",error)
+        }
+    }
+
+    async updatePost(slug , {title , content ,featuredImage , status , userId}){
+        try {
+            return await this.databases.updateDocument(
+                config.appwriteDataBaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service :: error :: ",error)
+        }
+    }
+    async deletePost(slug){
+        try {
+                await this.databases.deleteDocument(
+                config.appwriteDataBaseId,
+                config.appwriteCollectionId,
+                slug,
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite service :: error :: ",error)
+            return false
+        }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                config.appwriteDataBaseId,
+                config.appwriteCollectionId,
+                slug,
+            )
+        } catch (error) {
+            console.log("Appwrite service :: error :: ",error)
+
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status" , "active")]){
+        try {
+            return await this.databases.listDocuments(
+                config.appwriteDataBaseId,
+                config.appwriteCollectionId,
+                [
+                    queries,
+                ]
+            )
+        } catch (error) {
+            console.log("Appwrite service :: error :: ",error)
+
+        }
+    }
+}
+
+const service = new Services();
+
+export default service;
